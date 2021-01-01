7fc3f00e24add48b4471eb995afaee2b8ee973e5
egehan205 <egehankarakose25@hotmail.com>
2021-01-01 13:37:29 +0300

    Initial Commit

---
+++ ForumApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForumApp
+//
+//  Created by Egehan Karaköse on 1.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
